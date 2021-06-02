14b3bcffceaaa335450f18bb2e7482c416c2b1a9
jaehong.lee <jaehong.lee@solarconnect.kr>
2021-06-02 12:00:22 +0900

    typescript 부분 변환

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense } from "react";
 import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 
-const ContextTodoContainer = lazy(() =>
-  import("./pages/Context/ContextTodoContainer")
+const ContextTodoContainer = lazy(
+  () => import("./pages/Context/ContextTodoContainer")
 );
-const RecoilTodoContainer = lazy(() =>
-  import("./pages/Recoil/RecoilTodoContainer")
+const RecoilTodoContainer = lazy(
+  () => import("./pages/Recoil/RecoilTodoContainer")
 );
 const MobxTodoContainer = lazy(() => import("./pages/Mobx/MobxTodoContainer"));
 function App() {
